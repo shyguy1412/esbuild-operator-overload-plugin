117545f578d916d5b7b678135df1610c7d918b80
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-02-21 22:50:01 +0100

    fix module declaration

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,14 @@
-export const OverloadPlugin: import('esbuild').Plugin;
+declare module 'OperatorSymbols' {
+  export type UnaryOperator = "-" | "+" | "!" | "~" | "typeof" | "void" | "delete";
+  export type UpdateOperator = "++" | "--";
+  export type BinaryOperator = "==" | "!=" | "===" | "!==" | "<" | "<=" | ">" | ">=" | "<<" | ">>" | ">>>" | "+" | "-" | "*" | "/" | "%" | "|" | "^" | "&" | "in" | "instanceof" | "**";
+  export type AssignmentOperator = "=" | "+=" | "-=" | "*=" | "/=" | "%=" | "<<=" | ">>=" | ">>>=" | "|=" | "^=" | "&=" | "**=" | "||=" | "&&=" | "??=";
+  export type LogicalOperator = "||" | "&&" | "??";
+  export type Operator = UnaryOperator | UpdateOperator | BinaryOperator | AssignmentOperator | LogicalOperator;
+  const OperatorSymbols: Record<Operator, symbol>;
+  export default OperatorSymbols;
+}
+
+declare module 'esbuild-operator-overload-plugin' {
+  export const OverloadPlugin: import('esbuild').Plugin;
+}
